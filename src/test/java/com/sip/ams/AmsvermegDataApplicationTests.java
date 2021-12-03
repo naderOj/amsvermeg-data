67051f14981e3414d68cee57a0cfe93fcdf7770e
Nader Ouerhani <nader.ourhani@sesame.com.tn>
2021-12-03 14:47:41 +0100

    first commit

---
+++ src/test/java/com/sip/ams/AmsvermegDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sip.ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsvermegDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
